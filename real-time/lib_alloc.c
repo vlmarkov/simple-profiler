f2f7a1d41805c7213c605db7376f471e4cc4ca6f
Vladislav <randyshughart54@gmail.com>
2018-09-08 16:34:09 +0700

    Rewrite whole library

--- real-time/lib_alloc.c
+++ real-time/lib_alloc.c
@@ -1,231 +1,207 @@
 #define _GNU_SOURCE
+
 #include <dlfcn.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/syscall.h>
+#include <assert.h>
+#include <pthread.h>
+#include <stdbool.h>
 
-/*
- * There is no recursive protection, it will segfault
- */
+
+#define RESERVED_SIZE 8192
 
 typedef struct
 {
-    long size;
-} header_t;
+    size_t size;
+} mem_header_t;
 
-static void  (*real_free)(void *ptr)                   = NULL;
-static void* (*real_malloc)(size_t size)               = NULL;
-static void* (*real_calloc)(size_t nmemb, size_t size) = NULL;
-static void* (*real_realloc)(void *ptr, size_t size)   = NULL;
+static uint8_t  reserved_memory[RESERVED_SIZE] = { 0 };
+static uint64_t allocated_bytes;
 
-static long long alloced_bytes;
+/*****************************************************************************/
+/* The keyword __thread: is used to define thread-local variables.           */
+/* This helps to avoid infinite recursive call                               */
+/*****************************************************************************/
+static __thread int no_hook;
 
+/*****************************************************************************/
+/* Hooking glibc function poninters                                          */
+/*****************************************************************************/
+static void *(*real_calloc) (size_t, size_t) = NULL;
+static void *(*real_malloc) (size_t)         = NULL;
+static void *(*real_realloc)(void *, size_t) = NULL;
+static void  (*real_free)   (void *)         = NULL;
 
-static void init(void)
+/*****************************************************************************/
+/* __attribute__((constructor)) syntax:                                      */
+/* This particular GCC syntax, when used with a function,                    */
+/* executes the same function at the startup of the program,                 */
+/* i.e before main() function.                                               */
+/*****************************************************************************/
+static void __attribute__((constructor)) init(void)
 {
-    real_malloc  = dlsym(RTLD_NEXT, "malloc");
-    real_calloc  = dlsym(RTLD_NEXT, "calloc");
-    real_realloc = dlsym(RTLD_NEXT, "realloc");
-    real_free    = dlsym(RTLD_NEXT, "free");
+    real_calloc  = (void *(*) (size_t, size_t)) dlsym(RTLD_NEXT, "calloc");
+    real_malloc  = (void *(*) (size_t))         dlsym(RTLD_NEXT, "malloc");
+    real_realloc = (void *(*) (void *, size_t)) dlsym(RTLD_NEXT, "realloc");
+    real_free    = (void  (*) (void *))         dlsym(RTLD_NEXT, "free");
 
-    alloced_bytes = 0;
+    no_hook = 0;
+    allocated_bytes = 0;
+
+    fprintf(stderr, "ADDRESS \tNAME    SIZE\tPOINTER  \tHEAP\n");
+}
+
+/*****************************************************************************/
+/* In multi-thread mode, dlsym() uses calloc() at the first time.            */
+/* - calloc() requires dlsym()                                               */
+/* - dlsym() requires calloc() … !!                                          */
+/* Set up special calloc() for the firs call of calloc().                    */
+/*****************************************************************************/
+static void *get_reserved_memory(size_t nmemb, size_t size)
+{
+    if ((nmemb * size) > RESERVED_SIZE)
+    {
+        fprintf(stderr, "<%s> Can't provide stack reserved memory\n", __FUNCTION__);
+        assert(0);
+    }
+
+    return reserved_memory;
+}
+
+static void *increase_heap_usage(void *ptr, const size_t size, const bool is_realloc)
+{
+    if (!ptr)
+    {
+        fprintf(stderr, "<%s> Can't get memory pointer\n", __FUNCTION__);
+        assert(0);
+    }
+
+    if (is_realloc)
+    {
+        size_t old_size = ((mem_header_t *)(ptr))->size;
+        ((mem_header_t *)(ptr))->size = size;
+        allocated_bytes -= old_size;
+        allocated_bytes += size;
+    }
+    else
+    {
+        ((mem_header_t *)(ptr))->size = size;
+        allocated_bytes += size;
+    }
+
+    return ptr + sizeof(mem_header_t);
+}
+
+static void *decrease_heap_usage(void *ptr)
+{
+    mem_header_t *mem_header = (mem_header_t *)(ptr - sizeof(mem_header_t));
+    if (!mem_header)
+    {
+        fprintf(stderr, "<%s> Can't get pointer size\n", __FUNCTION__);
+        assert(0);
+    }
+
+    allocated_bytes -= mem_header->size;
+
+    return ptr - sizeof(mem_header_t);
+}
+
+static size_t get_heap_usage_by_ptr(void *ptr)
+{
+    return ((mem_header_t *)(ptr))->size;
+}
+
+static void print_log(const char  *func,
+                      const void  *caller,
+                      const void  *addr,
+                      const size_t size)
+{
+    fprintf(stderr, "%p\t%s(%zu)\t%p\t%zu\n",
+        caller, func, size, addr, allocated_bytes);
 }
 
 void *malloc(size_t size)
 {
-    if (!real_malloc)
-        init();
+    if (no_hook)
+        return (*real_malloc)(size);
 
-    void     *p = NULL;
-    header_t *h = NULL;
+    no_hook = 1;
 
-    // Actual function call
-    p = real_malloc(size + sizeof(header_t));
-    if (!p)
+    void *ret_ptr = (*real_malloc)(size + sizeof(mem_header_t));
+    if (ret_ptr)
     {
-        return NULL;
+        ret_ptr = increase_heap_usage(ret_ptr, size, false);
     }
 
-    h = (header_t *)p;
-    if (!h)
-    {
-        fprintf(stderr, "<%s> Can't get pointer size\n", __FUNCTION__);
-        abort();
-    }
+    print_log("malloc  ", __builtin_return_address(0), ret_ptr, size);
 
-    h->size = size;
+    no_hook = 0;
 
-    alloced_bytes += h->size;
-
-    void *ptr = p + sizeof(header_t);
-    if (!ptr)
-    {
-        fprintf(stderr, "<%s> Can't get pointer\n", __FUNCTION__);
-        abort();
-    }
-
-    // Replace by your logging message
-    printf("<%s >\tptr[%p]\tsize(%ld)\tpid = %ld/%ld\n",
-           __FUNCTION__, ptr, h->size, (long)getpid(), syscall(SYS_gettid));
-
-    return ptr;
+    return ret_ptr;
 }
 
 void *calloc(size_t nmemb, size_t size)
 {
-    if (!real_calloc)
-        init();
+    if (real_calloc == NULL)
+        return get_reserved_memory(nmemb, size);
 
-    void     *p = NULL;
-    header_t *h = NULL;
+    if (no_hook)
+        return (*real_calloc)(nmemb, size);
+
+    no_hook = 1;
 
     // Replace calloc to malloc() + memset()
-    p = malloc((nmemb * size) + sizeof(header_t));
-    if (!p)
+    void *ret_ptr = (*real_malloc)((nmemb * size) + sizeof(mem_header_t));
+    if (ret_ptr)
     {
-        return NULL;
-    }
-    memset(p, 0, (nmemb * size) + sizeof(header_t));
-
-    h = (header_t *)p;
-    if (!h)
-    {
-        fprintf(stderr, "<%s> Can't get pointer size\n", __FUNCTION__);
-        abort();
+        memset(ret_ptr, 0, (nmemb * size) + sizeof(mem_header_t));
+        ret_ptr = increase_heap_usage(ret_ptr, (nmemb * size), false);
     }
 
-    h->size = (nmemb * size);
+    print_log("calloc  ", __builtin_return_address(0), ret_ptr, size);
 
-    alloced_bytes += h->size;
+    no_hook = 0;
 
-    void *ptr = p + sizeof(header_t);
-    if (!ptr)
-    {
-        fprintf(stderr, "<%s> Can't get pointer\n", __FUNCTION__);
-        abort();
-    }
-
-    // Replace by your logging message
-    printf("<%s >\tptr[%p]\tsize(%ld)\tpid = %ld/%ld\n",
-           __FUNCTION__, ptr, h->size, (long)getpid(), syscall(SYS_gettid));
-
-    return ptr;
+    return ret_ptr;
 }
 
 void *realloc(void *ptr, size_t size)
 {
-    if (!real_realloc)
-        init();
+    if (no_hook)
+        return (*real_realloc)(ptr, size);
 
-    void     *p = NULL;
-    header_t *h = NULL;
+    no_hook = 1;
 
-    void *realloced_ptr = NULL;
-
-    int new_pointer = 0;
-
+    bool is_realloc = false;
     if (ptr)
     {
-        // We try to realloc allocated pointer
-        p = ptr - sizeof(header_t);
-        if (!p)
-        {
-            fprintf(stderr, "<%s> Can't get pointer\n", __FUNCTION__);
-            abort();
-        }
+        is_realloc = true;
+        ptr = ptr - sizeof(mem_header_t);
     }
-    else
+
+    void *ret_ptr = (*real_realloc)(ptr, (size + sizeof(mem_header_t)));
+    if (ret_ptr)
     {
-        // If ptr is NULL, then the call is equivalent to malloc(size)
-        p = ptr;
-        new_pointer = 1;
-
-        if (size != 0)
-        {
-            size += sizeof(header_t);
-        }
+        ret_ptr = increase_heap_usage(ret_ptr, size, is_realloc);
     }
 
-    /*
-     * If size is equal to zero, and ptr is not NULL,
-     * then the call is equivalent to free(ptr).
-     */
-    if (ptr && size == 0)
-        fprintf(stderr, "<%s> Try to realloc to zero size\n", __FUNCTION__);
+    print_log("realloc ", __builtin_return_address(0), ret_ptr, size);
 
-    // Actual function call
-    p = real_realloc(p, size);
-    if (!p)
-    {
-        return p;
-    }
 
-    if (size != 0)
-    {
-        h = (header_t *)p;
-        if (!h)
-        {
-            fprintf(stderr, "<%s> Can't get pointer size\n", __FUNCTION__);
-            abort();
-        }
+    no_hook = 0;
 
-        if (new_pointer)
-        {
-            h->size = size - sizeof(header_t);
-            alloced_bytes += h->size;
-        }
-        else
-        {
-            h->size       += size;
-            alloced_bytes += size;
-        }
-
-        realloced_ptr = p + sizeof(header_t);
-    }
-
-    // Replace by your logging message
-    printf("<%s>\tptr[%p]\tsize(%ld)\tpid = %ld/%ld\n",
-           __FUNCTION__, realloced_ptr, h->size, (long)getpid(), syscall(SYS_gettid));
-
-    return realloced_ptr;
+    return ret_ptr;
 }
 
 void free(void *ptr)
 {
-    if (!real_free)
-        init();
-
-    void     *p = NULL;
-    header_t *h = NULL;
-
     if (!ptr)
-    {
         return;
-    }
 
-    p = ptr - sizeof(header_t);
-    if (!p)
-    {
-        fprintf(stderr, "<%s> Can't get pointer\n", __FUNCTION__);
-        abort();
-    }
+    ptr = decrease_heap_usage(ptr);
 
-    h = (header_t *)p;
-    if (!h)
-    {
-        fprintf(stderr, "<%s> Can't get pointer size\n", __FUNCTION__);
-        abort();
-    }
+    print_log("free    ", __builtin_return_address(0), ptr, get_heap_usage_by_ptr(ptr));
 
-    alloced_bytes -= h->size;
-
-    // Replace by your logging message
-    printf("<%s   >\tptr[%p]\tsize(%ld)\tpid = %ld/%ld\n",
-           __FUNCTION__, p, h->size, (long)getpid(), syscall(SYS_gettid));
-
-    // Actual function call
-    real_free(ptr);
+    (*real_free)(ptr);
 }
