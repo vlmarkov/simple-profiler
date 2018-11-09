be89b57f5ae738835ec8355e6d2645c4e796811a
Vladislav Markov <randyshughart54@gmail.com>
2018-11-09 20:07:26 +0700

    Fixed: hook-malloc source files

--- hook-malloc/run-it.sh
+++ hook-malloc/run-it.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-LD_PRELOAD=$PWD/build/lib_hook_malloc.so build/sample 2> $PWD/log
+LD_PRELOAD=$PWD/build/lib_hook_malloc.so build/sample 2> $PWD/build/log
 
 echo "Running: sample..."
-cat $PWD/log
+cat $PWD/build/log
 echo "Running: sample [complete]"
+echo ""
 
-LD_PRELOAD=$PWD/build/lib_hook_malloc.so build/leak-sample 2> $PWD/log
+LD_PRELOAD=$PWD/build/lib_hook_malloc.so build/leak-sample 2> $PWD/build/log
 
 echo "Running: leak-sample..."
-cat $PWD/log
+cat $PWD/build/log
 echo "Running: leak-sample [complete]"
