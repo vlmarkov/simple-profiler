8068fecde8c09667b1545037077f3ed8bea55606
Vladislav Markov <randyshughart54@gmail.com>
2018-11-09 21:22:28 +0700

    Fixed: indent

--- perf-event/src/main.c
+++ perf-event/src/main.c
@@ -22,16 +22,16 @@ static void usage(char const *argv0)
 /*****************************************************************************/
 static void sudo_check(char const *argv0)
 {
-	if (getuid() != 0 && geteuid() != 0)
-	{
-		fprintf(stderr, "usage: sudo %s <your-test-program>\n", argv0);
-		exit(-1);
-	}
+    if (getuid() != 0 && geteuid() != 0)
+    {
+        fprintf(stderr, "usage: sudo %s <your-test-program>\n", argv0);
+        exit(-1);
+    }
 }
 
 int main(int argc, char **argv)
 {
-	sudo_check(argv[0]);
+    sudo_check(argv[0]);
 
     if (argc < 2)
     {
